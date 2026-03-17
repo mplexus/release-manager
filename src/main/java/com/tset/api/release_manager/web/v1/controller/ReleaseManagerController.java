d159230094512190a3f586be6d3f4c85a5033f1f
Michael Plexousakis <adm_micple01@cramo.com>
2026-03-17 23:11:07 +0100

    Add web controller

---
+++ src/main/java/com/tset/api/release_manager/web/v1/controller/ReleaseManagerController.java
@@ -0,0 +1,40 @@
+package com.tset.api.release_manager.web.v1.controller;
+
+import com.tset.api.release_manager.data.ServiceReleaseRepository;
+import com.tset.api.release_manager.data.model.ServiceRelease;
+import com.tset.api.release_manager.service.ReleaseService;
+import com.tset.api.release_manager.web.v1.model.ServiceReleaseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
+
+@Validated
+@RequiredArgsConstructor
+@RestController
+@RequestMapping(produces = APPLICATION_JSON_VALUE, path = "/api/v1/releases")
+public class ReleaseManagerController {
+
+    private final ReleaseService releaseService;
+
+    @GetMapping("/services")
+    public ResponseEntity<List<ServiceRelease>> getServices(
+            @RequestParam(name = "systemVersion", required = false)
+            Long systemVersion) {
+
+        return ResponseEntity.ok(releaseService.getServiceReleasesBySystemVersion(systemVersion));
+    }
+
+    @PostMapping("/depploy")
+    public ResponseEntity<Long> addService(
+            @RequestBody ServiceReleaseDto serviceRelease) {
+
+        Long version = releaseService.addServiceRelease(serviceRelease);
+        return ResponseEntity.ok(version);
+    }
+}
